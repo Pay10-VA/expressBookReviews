7d45e788c74cab4e563b9d93c8cb1c14118db287
Payton Viloria <paytonviloria@Paytons-MacBook-Pro.local>
2024-08-09 15:25:05 -0600

    Finished task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,7 +27,15 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = Number(req.params.isbn);
+  let resultingBook;
+  Object.keys(books).forEach((key) => {
+    if(books[key].isbn === isbn) {
+      resultingBook = books[key];
+      return;
+    }
+  }); 
+  return res.status(200).json({book: resultingBook});
  });
   
 // Get book details based on author
