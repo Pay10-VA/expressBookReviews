4bbc7070c74767c4334a286a4bfff423b46aab13
Payton Viloria <paytonviloria@Paytons-MacBook-Pro.local>
2024-08-09 15:16:06 -0600

    Completed task 1

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,7 +13,15 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let arr = [];
+  Object.keys(books).forEach((key) => {
+    arr.push(books[key])
+  });
+
+
+  return res.status(200).json({
+    books: arr
+  });
 });
 
 // Get book details based on ISBN
